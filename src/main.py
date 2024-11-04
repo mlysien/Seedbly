2248c0e66d62337977eba1cce030578a437b9ad0
Mateusz Łysień <mateusz.lysien@homeverest.pl>
2024-11-04 22:26:38 +0100

    add termcolor package

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
-﻿from loader import load_settings
+﻿from termcolor import colored
+from loader import load_settings
 
 if __name__ == '__main__':
-
     # load settings
     settings = load_settings()
 
-    print(f'Settings loaded {settings.engine}')
+    print(colored('\u2713', 'green'), colored('Settings loaded successful!', 'white'))
+    

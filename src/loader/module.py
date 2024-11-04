040513a94e6e9e3ba17a6a771b79b3c1858e7a2b
Mateusz Łysień <mateusz.lysien@homeverest.pl>
2024-11-04 22:45:17 +0100

    add error handling

--- src/loader/module.py
+++ src/loader/module.py
@@ -1,9 +1,9 @@
 import json
-
 from loader import *
+from json import JSONDecodeError
+from termcolor import colored
 
 settings_file_path = 'settings.json'
-
 
 def as_settings(dct):
     return Settings(dct['engine'], dct['stencil'], dct['size'], dct['connection-string'])
@@ -11,5 +11,16 @@ def as_settings(dct):
 
 def load_settings() -> Settings:
     """Loads settings from local file and parse to ``Settings`` entity."""
-    json_file = open(settings_file_path, mode="r")
-    return json.load(json_file, object_hook=as_settings)
+    try:
+        json_file = open(settings_file_path, mode="r")
+        loaded_settings = json.load(json_file, object_hook=as_settings)
+
+        print(colored('\u2713', 'green'), colored('Settings loaded successful!', 'white'))
+
+        return loaded_settings
+
+    except FileNotFoundError:
+        print(colored(f'File "{settings_file_path}" not found!', 'red'))
+
+    except JSONDecodeError:
+        print(colored(f'Settings file has invalid structure!', 'red'))
--- src/main.py
+++ src/main.py
@@ -4,6 +4,3 @@ from loader import load_settings
 if __name__ == '__main__':
     # load settings
     settings = load_settings()
-
-    print(colored('\u2713', 'green'), colored('Settings loaded successful!', 'white'))
-    

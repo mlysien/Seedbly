fd021b3a4fd67b11886a83ae161b299daed918b7
Mateusz Łysień <mateusz.lysien@homeverest.pl>
2024-11-04 23:59:04 +0100

    Update gitworkflows

--- src/loader/module.py
+++ src/loader/module.py
@@ -1,15 +1,28 @@
 import json
 from loader import *
 from json import JSONDecodeError
+
 from termcolor import colored
+<<<<<<< Updated upstream
+=======
+
+from loader import settings
+>>>>>>> Stashed changes
 
 settings_file_path = 'settings.json'
 
+<<<<<<< Updated upstream
 def as_settings(dct):
     return Settings(dct['engine'], dct['stencil'], dct['size'], dct['connection-string'])
+=======
+
+def as_settings(dct) -> settings.Settings:
+    """Loads settings from local file and parse to ``Settings`` entity."""
+    return settings.Settings(dct['engine'], dct['stencil'], dct['size'], dct['connection-string'])
+>>>>>>> Stashed changes
 
 
-def load_settings() -> Settings:
+def load_settings() -> settings.Settings:
     """Loads settings from local file and parse to ``Settings`` entity."""
     try:
         json_file = open(settings_file_path, mode="r")
@@ -20,7 +33,16 @@ def load_settings() -> Settings:
         return loaded_settings
 
     except FileNotFoundError:
+<<<<<<< Updated upstream
         print(colored(f'File "{settings_file_path}" not found!', 'red'))
 
     except JSONDecodeError:
         print(colored(f'Settings file has invalid structure!', 'red'))
+=======
+        print(colored(f'File "{SETTINGS_FILE_PATH}" not found!', 'red'))
+        return settings.Settings('', '', 1, '')
+
+    except JSONDecodeError:
+        print(colored('Settings file has invalid structure!', 'red'))
+        return settings.Settings('', '', 1, '')
+>>>>>>> Stashed changes

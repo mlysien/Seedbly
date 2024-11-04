a1c07921ff81edf3ea845433eb6f8293634361f9
Mateusz Łysień <mateusz.lysien@homeverest.pl>
2024-11-04 20:15:54 +0100

    clean up main.py

--- src/main.py
+++ src/main.py
@@ -1,20 +1,5 @@
-﻿from engine.module import generate
+﻿def print_hi(name):
+    print(f'Hi, {name}!')
 
-
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}!')  # Press Ctrl+F8 to toggle the breakpoint.
-    generate();
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('Seedbly')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/

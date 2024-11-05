3b15ae4cafe43856395ebb22f3a03cfa056b812a
Mateusz Łysień <mateusz.lysien@homeverest.pl>
2024-11-05 17:59:04 +0100

    restore some fucked stuff

--- src/loader/settings.py
+++ src/loader/settings.py
@@ -1,15 +1,9 @@
-<<<<<<< Updated upstream
-﻿class Settings:
-    """Contains application settings."""
-=======
-﻿"""
+"""
 Contains entity represents application settings.
 """
 
 class Settings:
     """Represents application settings."""
-
->>>>>>> Stashed changes
     def __init__(self, engine, stencil, size, connection_string):
         self.engine = engine
         self.stencil = stencil

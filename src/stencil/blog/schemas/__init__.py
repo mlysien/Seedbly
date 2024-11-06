0a79fac2a340bba17a7e34975f9b1ab5e1150cf1
Mateusz Łysień <mateusz.lysien@homeverest.pl>
2024-11-06 22:09:34 +0100

    add stencil scripts paths

---
+++ src/stencil/blog/schemas/__init__.py
@@ -0,0 +1,3 @@
+﻿from stencil.blog.schemas.main import BLOG_SCHEMA_SCRIPT_PATH_MSSQL
+from stencil.blog.schemas.main import BLOG_SCHEMA_SCRIPT_PATH_SQLITE
+from stencil.blog.schemas.main import BLOG_SCHEMA_SCRIPT_PATH_POSTGRESQL

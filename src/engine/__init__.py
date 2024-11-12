a45a1975dbadd1881ebbd60472f8e2cacfb981f0
Mateusz Łysień <mateusz.lysien@homeverest.pl>
2024-11-12 21:02:39 +0100

    update imports

--- src/engine/__init__.py
+++ src/engine/__init__.py
@@ -2,3 +2,4 @@
 from engine.core import DatabaseEngine
 from engine.drivers import create_sqlite_schema
 from engine.drivers import create_postgresql_schema
+from engine.drivers import create_mssql_schema
--- src/engine/drivers/__init__.py
+++ src/engine/drivers/__init__.py
@@ -1,2 +1,3 @@
 ﻿from engine.drivers.sqlite import create_sqlite_schema
 from engine.drivers.postgresql import create_postgresql_schema
+from engine.drivers.mssql import  create_mssql_schema

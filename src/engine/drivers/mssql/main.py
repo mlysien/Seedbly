06a3e2d8cd4de23ba9eeb3eaddb49da61882b965
Mateusz Łysień <mateusz.lysien@homeverest.pl>
2024-11-12 21:15:41 +0100

    implement create_mssql_schema function

---
+++ src/engine/drivers/mssql/main.py
@@ -0,0 +1,30 @@
+﻿from pyodbc import connect
+from termcolor import colored
+
+
+def create_mssql_schema(connection_parameters: dict[str, str], mssql_script):
+    """
+    Creates MSSQL Server database schema
+    :param connection_parameters: Collection of connection parameters
+    :param mssql_script: MSSQL schema generator script
+    """
+    driver = 'SQL Server'
+    server = connection_parameters['server']
+    port = connection_parameters['port']
+    database = connection_parameters['database']
+    username = connection_parameters['username']
+    password = connection_parameters['password']
+
+    connection_string = (f'DRIVER={driver};'
+                         f'SERVER={server},{port};'
+                         f'DATABASE={database};'
+                         f'UID={username};'
+                         f'PWD={password}')
+
+    connection = connect(connection_string)
+
+    cursor = connection.cursor()
+    cursor.execute(mssql_script)
+    connection.commit()
+
+    print(colored('\u2713', 'green'), colored('MSSQL schema created successfully!', 'white'))

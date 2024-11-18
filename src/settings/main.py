ad5a094d24dc0ec5e089da5de46210bb2905cb11
Mateusz Łysień <mateusz.lysien@homeverest.pl>
2024-11-18 17:46:33 +0100

    add connection params provider

--- src/settings/main.py
+++ src/settings/main.py
@@ -1,6 +1,7 @@
 import inquirer
-from engine import DatabaseEngine
-from engine.core.main import DatabaseSchemas, DatabaseSchemaSizes
+from engine import DatabaseEngine, DatabaseSchemaSizes
+from engine.core import DatabaseSchemas
+
 
 def provide_settings():
     """
@@ -29,3 +30,30 @@ def provide_settings():
 
     # return setup parameters
     return inquirer.prompt(setup)
+
+def provide_connection_params(engine):
+    """
+    Displays interactive menu for provide required settings.
+    """
+
+    if engine == DatabaseEngine.SQLITE.value:
+        setup = [
+            inquirer.Text("database", message="Enter SQLite database name", default="local"),
+        ]
+
+        # return connection parameters for sqlite
+        return inquirer.prompt(setup)
+
+    if engine == DatabaseEngine.MSSQL.value:
+        setup = [
+            inquirer.Text("server", message="Server", default="localhost"),
+            inquirer.Text("port", message="Port", default="1443"),
+            inquirer.Text("database", message="Database", default="main"),
+            inquirer.Text("username", message="Username", default="sa"),
+            inquirer.Text("password", message="Password"),
+        ]
+
+        # return connection parameters for mssql
+        return inquirer.prompt(setup)
+
+    return -1

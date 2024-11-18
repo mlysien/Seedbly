a0929a237396dd4f6b4bcf861bf9a70fda9e350e
Mateusz Łysień <mateusz.lysien@homeverest.pl>
2024-11-18 16:47:55 +0100

    update imports

--- src/schemas/blog/__init__.py
+++ src/schemas/blog/__init__.py
@@ -1,1 +1,3 @@
-﻿from schemas.blog.scripts import BLOG_SCHEMA_SCRIPT_PATH_SQLITE
+﻿from schemas.blog.scripts.main import BLOG_SCHEMA_SCRIPT_PATH_SQLITE
+from schemas.blog.scripts.main import BLOG_SCHEMA_SCRIPT_PATH_MSSQL
+from schemas.blog.scripts.main import BLOG_SCHEMA_SCRIPT_PATH_POSTGRESQL
